55f46275916aaedb8e0200c1dee6341aceb8db86
safronov <ilya.safa1337w@gmail.com>
2023-10-06 16:33:44 +0500

    Added some unit tests in the LinkedListImplTest for contains method in the LinkedListImpl

--- src/test/kotlin/linked_list/LinkedListImplTest.kt
+++ src/test/kotlin/linked_list/LinkedListImplTest.kt
@@ -3,11 +3,12 @@ package linked_list
 import org.junit.Assert
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class LinkedListImplTest {
 
     @Test
-    fun `test add item, should return added item and change item size`() {
+    fun `test, add item, should return added item and change item size`() {
         val linkedListImpl = LinkedListImpl<Int>()
         val addedItem = 5
         Assert.assertEquals(addedItem, linkedListImpl.add(addedItem))
@@ -15,20 +16,20 @@ class LinkedListImplTest {
     }
 
     @Test
-    fun `test get item, should return item by index`() {
+    fun `test, get item, should return item by index`() {
         val linkedListImpl = LinkedListImpl<Int>()
         val addedItem = 5
         Assert.assertEquals(addedItem, linkedListImpl.add(addedItem))
     }
 
     @Test(expected = IllegalStateException::class)
-    fun `test get item from empty list`() {
+    fun `test, get item from empty list`() {
         val linkedListImpl = LinkedListImpl<Int>()
         linkedListImpl.get(0)
     }
 
     @Test(expected = IllegalStateException::class)
-    fun `test get item by wrong index, should throw a exception`() {
+    fun `test, get item by wrong index, should throw a exception`() {
         val linkedListImpl = LinkedListImpl<Int>()
         val addedItem = 1
         Assert.assertTrue(addedItem == linkedListImpl.add(addedItem))
@@ -36,7 +37,7 @@ class LinkedListImplTest {
     }
 
     @Test
-    fun `test delete item by index, should delete and then return deleted item`() {
+    fun `test, delete item by index, should delete and then return deleted item`() {
         val linkedListImpl = LinkedListImpl<Int>()
         val addedItem = 1
         Assert.assertTrue(addedItem == linkedListImpl.add(addedItem))
@@ -45,7 +46,7 @@ class LinkedListImplTest {
     }
 
     @Test(expected = IllegalStateException::class)
-    fun `test delete item by wrong index, should throw a exception`() {
+    fun `test, delete item by wrong index, should throw a exception`() {
         val linkedListImpl = LinkedListImpl<Int>()
         val addedItem = 1
         Assert.assertTrue(addedItem == linkedListImpl.add(addedItem))
@@ -53,7 +54,7 @@ class LinkedListImplTest {
     }
 
     @Test
-    fun `test delete twice, should delete two items`() {
+    fun `test, delete twice, should delete two items`() {
         val linkedListImpl = LinkedListImpl<Int>()
         val addedItem = 1
         val addedItem2 = 2
@@ -65,7 +66,7 @@ class LinkedListImplTest {
     }
 
     @Test
-    fun `test add and delete item twice`() {
+    fun `test, add and delete item twice`() {
         val linkedListImpl = LinkedListImpl<Int>()
         val addedItem = 1
         linkedListImpl.add(addedItem)
@@ -77,7 +78,7 @@ class LinkedListImplTest {
     }
 
     @Test
-    fun `test clear all items, should delete all items`() {
+    fun `test, clear all items, should delete all items`() {
         val linkedListImpl = LinkedListImpl<Int>()
         val addedItem = 1
         linkedListImpl.add(addedItem)
@@ -89,7 +90,7 @@ class LinkedListImplTest {
     }
 
     @Test(expected = IllegalStateException::class)
-    fun `test get item after clear all items, should throw exception`() {
+    fun `test, get item after clear all items, should throw exception`() {
         val linkedListImpl = LinkedListImpl<Int>()
         val addedItem = 1
         linkedListImpl.add(addedItem)
@@ -101,7 +102,7 @@ class LinkedListImplTest {
     }
 
     @Test
-    fun `test add too much items, should get item at some position without null`() {
+    fun `test, add too much items, should get item at some position without null`() {
         val linkedListImpl = LinkedListImpl<Int>()
         val addedItem = 1
         repeat(200) {
@@ -114,7 +115,7 @@ class LinkedListImplTest {
     }
 
     @Test
-    fun `test add items after that get all items and delete all items twice, should works correctly`() {
+    fun `test, add items after that get all items and delete all items twice, should works correctly`() {
         val linkedListImpl = LinkedListImpl<Int>()
         val addedItem = 1
         for (i in 0 .. 1) {
@@ -126,6 +127,32 @@ class LinkedListImplTest {
             }
             linkedListImpl.clearAll()
         }
+    }
+
+    @Test
+    fun `test, is contains some item in the list, should return true`() {
+        val linkedListImpl = LinkedListImpl<Int>()
+        val addedItem = 1
+        linkedListImpl.add(addedItem)
+        assertTrue(linkedListImpl.contains(addedItem))
+    }
+
+    @Test
+    fun `test, is contains some item in the list, should return false 'cause item is not in the list`() {
+        val linkedListImpl = LinkedListImpl<Int>()
+        val addedItem = 1
+        linkedListImpl.add(999)
+        assertTrue(!(linkedListImpl.contains(addedItem)))
+    }
+
+    @Test
+    fun `test, is contains some item in the list, should return true also when there are some the same items`() {
+        val linkedListImpl = LinkedListImpl<Int>()
+        val addedItem = 1
+        repeat(10) {
+            linkedListImpl.add(addedItem)
+        }
+        assertTrue(linkedListImpl.contains(addedItem))
     }
 
 }
